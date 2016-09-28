457a7e4c33d05a9128378c7994f8b19313bd81f1
Priya Menon <menon94priya@gmail.com>
2016-09-28 10:22:04 -0700

    Add files via upload

---
+++ BloggController.java
@@ -0,0 +1,74 @@
+package com.niit.collaborationwebsite.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.niit.collaborationwebsite.DAO.BloggDAO;
+import com.niit.collaborationwebsite.model.Blogg;
+
+
+@RestController
+public class BloggController {
+
+	@Autowired
+	Blogg blogg;
+	
+	@Autowired
+	BloggDAO bloggDAO;
+	
+	@GetMapping("/bloggs/")
+	public ResponseEntity<List<Blogg>> listAllBloggs() {
+		List<Blogg> listOfBloggs = bloggDAO.listBlogg();
+		if (listOfBloggs.isEmpty()) {
+			return new ResponseEntity <List<Blogg>> (HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity < List < Blogg >> (listOfBloggs, HttpStatus.OK);
+	}
+
+	@PostMapping("/Blogg/")
+	public ResponseEntity<Void> createBlogg(@RequestBody Blogg blogg,
+			UriComponentsBuilder ucBuilder) {
+		if (bloggDAO.getBlogg(blogg.getId()) != null) {
+
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+
+		}
+
+		/*
+		 * role.setId("ROLE_USER"); role.setName("ROLE_USER");
+		 */
+		bloggDAO.saveOrUpdateBlogg(blogg);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(ucBuilder.path("Blogg/{id}/").buildAndExpand(blogg.getId()).toUri());
+		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+
+	}
+	
+	@PutMapping("/Blogg/{id}")
+	public ResponseEntity<Blogg> updateBlogg(@PathVariable("id") String id,
+			@RequestBody Blogg blogg) {
+		
+		if (bloggDAO.getBlogg(id) == null) {
+			return new ResponseEntity<Blogg>(HttpStatus.NOT_FOUND);
+		}
+		blogg.setId(id);
+
+		
+		bloggDAO.saveOrUpdateBlogg(blogg);
+
+		return new ResponseEntity<Blogg>(blogg, HttpStatus.OK);
+
+	}
+}
---
+++ ForumController.java
@@ -0,0 +1,75 @@
+package com.niit.collaborationwebsite.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.niit.collaborationwebsite.DAO.ForumDAO;
+import com.niit.collaborationwebsite.model.Forum;
+
+
+@RestController
+public class ForumController {
+
+	@Autowired
+	Forum forum;
+	
+	@Autowired
+	ForumDAO forumDAO;
+	
+	@GetMapping("/forums/")
+	public ResponseEntity<List<Forum>> listAllForums() {
+		List<Forum> listOfForums = forumDAO.listForum();
+		if (listOfForums.isEmpty()) {
+			return new ResponseEntity <List<Forum>> (HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity < List < Forum >> (listOfForums, HttpStatus.OK);
+	}
+
+	@PostMapping("/Forum/")
+	public ResponseEntity<Void> createForum(@RequestBody Forum forum,
+			UriComponentsBuilder ucBuilder) {
+		if (forumDAO.getForum(forum.getId()) != null) {
+
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+
+		}
+
+		/*
+		 * role.setId("ROLE_USER"); role.setName("ROLE_USER");
+		 */
+		forumDAO.saveOrUpdateForum(forum);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(ucBuilder.path("Forum/{id}/").buildAndExpand(forum.getId()).toUri());
+		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+
+	}
+	
+	@PutMapping("/Forum/{id}")
+	public ResponseEntity<Forum> updateForum(@PathVariable("id") String id,
+			@RequestBody Forum forum) {
+		
+		if (forumDAO.getForum(id) == null) {
+			return new ResponseEntity<Forum>(HttpStatus.NOT_FOUND);
+		}
+		forum.setId(id);
+
+		
+		forumDAO.saveOrUpdateForum(forum);
+
+		return new ResponseEntity<Forum>(forum, HttpStatus.OK);
+
+	}
+	
+}
